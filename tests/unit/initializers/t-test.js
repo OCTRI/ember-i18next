febf46d43a1d197fc8cb779cdae8e3f125aa85aa
Heath Harrelson <harrelst@ohsu.edu>
2015-01-21 15:22:27 -0800

    Initializer Tests
    Test the basic workings of the initializer -- loading the i18next
    library and injecting it into Ember classes.

---
+++ tests/unit/initializers/t-test.js
@@ -0,0 +1,62 @@
+import Ember from 'ember';
+import { initialize } from 'dummy/initializers/t';
+
+var container, application;
+
+module('TInitializer', {
+  setup: function() {
+    Ember.run(function() {
+      application = Ember.Application.create();
+      container = application.__container__;
+      application.deferReadiness();
+      initialize(container, application);
+    });
+  }
+});
+
+test('stream is created', function() {
+  expect(2);
+  ok(application.localeStream);
+  ok(application.localeStream.isStream);
+});
+
+test('i18n is attached to application', function () {
+  expect(1);
+  ok(application.i18n);
+});
+
+test('t is injected into routes', function () {
+  expect(2);
+
+  var TestRoute = Ember.Route.extend({});
+  container.register('route:test', TestRoute);
+
+  var testRouteInstance = container.lookup('route:test');
+
+  ok(testRouteInstance.t);
+  ok(typeof testRouteInstance.t === 'function');
+});
+
+test('t is injected into controllers', function () {
+  expect(2);
+
+  var TestController = Ember.Controller.extend({});
+  container.register('controller:test', TestController);
+
+  var testControllerInstance = container.lookup('controller:test');
+
+  ok(testControllerInstance.t);
+  ok(typeof testControllerInstance.t === 'function');
+});
+
+test('t is injected into components', function () {
+  expect(2);
+
+  var TestComponent = Ember.Component.extend({});
+  container.register('component:test', TestComponent);
+
+  var testComponentInstance = container.lookup('component:test');
+
+  ok(testComponentInstance.t);
+  ok(typeof testComponentInstance.t === 'function');
+});
