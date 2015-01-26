0af73a93cf5d4d94e50af9db2ff94a667ecc2555
Heath Harrelson <harrelst@ohsu.edu>
2015-01-26 11:14:03 -0800

    Translation Tests
    Add failing tests of the t() translation function.

---
+++ tests/unit/helpers/t-test.js
@@ -0,0 +1,11 @@
+import {
+  t
+} from 'dummy/helpers/t';
+
+module('THelper');
+
+// Replace this with your real tests.
+test('it works', function() {
+  var result = t(42);
+  ok(result);
+});
--- tests/unit/initializers/t-test.js
+++ tests/unit/initializers/t-test.js
@@ -1,10 +1,13 @@
 import Ember from 'ember';
 import { initialize } from 'dummy/initializers/t';
 
-var container, application;
+var container, application, testKey, testTranslation;
 
 module('TInitializer', {
   setup: function() {
+    testKey = 'test';
+    testTranslation = 'test output';
+
     Ember.run(function() {
       application = Ember.Application.create();
       container = application.__container__;
@@ -26,37 +29,52 @@ test('i18n is attached to application', function () {
 });
 
 test('t is injected into routes', function () {
-  expect(2);
+  expect(3);
 
-  var TestRoute = Ember.Route.extend({});
+  var TestRoute = Ember.Route.extend({
+    testProp: function() {
+      return this.t(testKey);
+    }.property()
+  });
   container.register('route:test', TestRoute);
 
   var testRouteInstance = container.lookup('route:test');
 
   ok(testRouteInstance.t);
   ok(typeof testRouteInstance.t === 'function');
+  equal(testRouteInstance.get('testProp'), testTranslation);
 });
 
 test('t is injected into controllers', function () {
-  expect(2);
+  expect(3);
 
-  var TestController = Ember.Controller.extend({});
+  var TestController = Ember.Controller.extend({
+    testProp: function () {
+      return this.t(testKey);
+    }.property()
+  });
   container.register('controller:test', TestController);
 
   var testControllerInstance = container.lookup('controller:test');
 
   ok(testControllerInstance.t);
   ok(typeof testControllerInstance.t === 'function');
+  equal(testControllerInstance.get('testProp'), testTranslation);
 });
 
 test('t is injected into components', function () {
-  expect(2);
+  expect(3);
 
-  var TestComponent = Ember.Component.extend({});
+  var TestComponent = Ember.Component.extend({
+    testProp: function () {
+      return this.t(testKey);
+    }.property()
+  });
   container.register('component:test', TestComponent);
 
   var testComponentInstance = container.lookup('component:test');
 
   ok(testComponentInstance.t);
   ok(typeof testComponentInstance.t === 'function');
+  equal(testComponentInstance.get('testProp'), testTranslation);
 });
