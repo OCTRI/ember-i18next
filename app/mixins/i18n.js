5538574537e7261aa9973035af4e82d9678abefc
Heath Harrelson <harrelst@ohsu.edu>
2015-04-28 15:42:01 -0700

    Mixin documentation comments

--- app/mixins/i18n.js
+++ app/mixins/i18n.js
@@ -1,5 +1,14 @@
 import Ember from 'ember';
 
+/**
+ * Mixes internationalization (i18n) functionality from the i18next library into
+ * the extending class. This mixin will add the following properties:
+ *
+ * @property {I18nService} i18n - the application's i18n service
+ * @property {Function} t - a shortcut to the i18n service's `t()` method, for
+ *   convenience.
+ * @see services/i18n
+ */
 var I18nMixin = Ember.Mixin.create({
   i18n: Ember.inject.service(),
 
