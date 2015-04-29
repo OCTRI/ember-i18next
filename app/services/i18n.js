3f3cb5a5473aa4189783d1159b67362ffb35dd05
Heath Harrelson <harrelst@ohsu.edu>
2015-04-28 17:00:24 -0700

    Library initialization in the service (WIP)

--- app/services/i18n.js
+++ app/services/i18n.js
@@ -1,10 +1,43 @@
 import Ember from 'ember';
+import config from '../config/environment';
 import { read, readHash } from 'ember-i18next/utils/stream';
 
 /**
  * A service that exposes functionality from i18next.
  */
 var I18nService = Ember.Service.extend({
+  initLibraryAsync: function () {
+    var application = this.container.lookup('application:main');
+    var options = config.i18nextOptions || {};
+    var i18next = window.i18n;
+
+    Ember.assert('i18next was not found. Check your bower.json file.', i18next);
+
+    var isThennable = function (obj) {
+      return obj && obj.then && typeof obj.then === 'function';
+    };
+
+    if (!i18next.initialized) {
+      return Ember.RSVP.Promise(function (resolve, reject) {
+        var initResponse = i18next.init(options);
+
+        if (isThennable(initResponse)) {
+          initResponse.then(function () {
+            application.set('locale', i18next.lng());
+            resolve(i18next);
+          }, function (val) {
+            Ember.warn('i18next.init() rejected with value: ' + val);
+            reject(val);
+          });
+        } else {
+          Ember.warn('The response from i18next.init() was not thennable.');
+          resolve(i18next);
+        }
+      });
+    }
+
+  },
+
   /**
    * A streamified version of the i18next `t()` function.
    *
