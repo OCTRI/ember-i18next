942f940c0a81e12dacef351d1800474ddcc41490
Maurice Kleine <info@mauricekleine.nl>
2017-10-23 17:07:42 +0200

    remove bower imports from index.js

--- index.js
+++ index.js
@@ -3,8 +3,4 @@
 
 module.exports = {
   name: 'ember-i18next',
-  included: function (app) {
-    app.import('bower_components/i18next/i18next.js');
-    app.import('bower_components/i18next-xhr-backend/i18nextXHRBackend.js');
-  }
 };
