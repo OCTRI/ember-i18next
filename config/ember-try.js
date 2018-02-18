e879e3855191dbf83faa0f0fe9e34a7659ef0628
Heath Harrelson <harrelst@ohsu.edu>
2018-02-17 22:46:54 -0800

    Update Ember
    Update to Ember 2.18

--- config/ember-try.js
+++ config/ember-try.js
@@ -3,6 +3,22 @@ module.exports = {
   useYarn: true,
   scenarios: [
     {
+      name: 'ember-lts-2.12',
+      npm: {
+        devDependencies: {
+          'ember-source': '~2.12.0'
+        }
+      }
+    },
+    {
+      name: 'ember-lts-2.16',
+      npm: {
+        devDependencies: {
+          'ember-source': '~2.16.0'
+        }
+      }
+    },
+    {
       name: 'ember-release',
       bower: {
         dependencies: {
@@ -52,6 +68,12 @@ module.exports = {
       }
     },
     {
+      name: 'ember-default',
+      npm: {
+        devDependencies: {}
+      }
+    },
+    {
       name: 'ember-1.13',
       bower: {
         dependencies: {
@@ -61,14 +83,6 @@ module.exports = {
       npm: {
         devDependencies: {
           'ember-source': null
-        }
-      }
-    },
-    {
-      name: 'ember-2.16',
-      npm: {
-        devDependencies: {
-          'ember-source': '~2.16.0'
         }
       }
     },
