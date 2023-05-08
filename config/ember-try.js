a4a6cfe536c0efbef1b1b7c637db2c2e3495d2b4
Heath Harrelson <heath.harrelson@gmail.com>
2023-05-07 21:35:43 -0700

    Passing Tests
    Get tests passing on Ember 3 and Ember 4.

--- config/ember-try.js
+++ config/ember-try.js
@@ -8,14 +8,6 @@ module.exports = async function () {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-3.24',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.24.3',
-          },
-        },
-      },
-      {
         name: 'ember-lts-3.28',
         npm: {
           devDependencies: {
@@ -24,60 +16,45 @@ module.exports = async function () {
         },
       },
       {
+        name: 'ember-lts-4.4',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.4.0',
+            'ember-auto-import': '^2.6.3',
+            webpack: '^5.78.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.8',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.8.0',
+            'ember-auto-import': '^2.6.3',
+            webpack: '^5.78.0',
+          },
+        },
+      },
+      {
         name: 'ember-release',
         npm: {
           devDependencies: {
             'ember-source': await getChannelURL('release'),
+            'ember-auto-import': '^2.6.3',
+            webpack: '^5.78.0',
           },
         },
       },
-      {
-        name: 'ember-beta',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('beta'),
-          },
-        },
-      },
-      {
-        name: 'ember-canary',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('canary'),
-          },
-        },
-      },
-      {
-        name: 'ember-default-with-jquery',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true,
-          }),
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^1.1.0',
-          },
-        },
-      },
-      {
-        name: 'ember-classic',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'application-template-wrapper': true,
-            'default-async-observers': false,
-            'template-only-glimmer-components': false,
-          }),
-        },
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.28.0',
-          },
-          ember: {
-            edition: 'classic',
-          },
-        },
-      },
+      // {
+      //   name: 'ember-beta',
+      //   npm: {
+      //     devDependencies: {
+      //       'ember-source': await getChannelURL('beta'),
+      //       'ember-auto-import': '^2.6.3',
+      //       'webpack': '^5.78.0'
+      //     },
+      //   },
+      // },
       embroiderSafe(),
       embroiderOptimized(),
     ],
